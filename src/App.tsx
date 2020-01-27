1e24ba292f920e5b6a22565ccad4f44fe0a0a2ae
awaji-daisuke <awaji_daisuke@intec.co.jp>
2020-01-27 12:51:57 +0900

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import BookShelf from "./organisms/BookShelf";
-import IsbnScanner from "./organisms/IsbnScanner";
+// import IsbnScanner from "./organisms/IsbnScanner";
+import Button from '@material-ui/core/Button';
 
 const App: React.FC = () => {
   return (
     <div>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
       <BookShelf />
       {/* <IsbnScanner /> */}
     </div>
