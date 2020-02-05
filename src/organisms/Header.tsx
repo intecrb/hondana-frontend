23c307b432a57b5b22c90dc8f5889e753d544218
awaji-daisuke <awaji_daisuke@intec.co.jp>
2020-02-05 12:33:39 +0900

    initialize SideBar

--- src/organisms/Header.tsx
+++ src/organisms/Header.tsx
@@ -1,35 +1,147 @@
 import React from "react";
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Button
-} from "@material-ui/core";
+import clsx from "clsx";
+import { AppBar, Toolbar, IconButton, Button, Drawer } from "@material-ui/core";
 import { HomeIcon } from "../atoms/HomeIcon";
+import MenuIcon from "@material-ui/icons/Menu";
 
-interface IProps {}
+import {
+  createStyles,
+  makeStyles,
+  useTheme,
+  Theme
+} from "@material-ui/core/styles";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import { SideMenuIcons } from "../molecules/SideMenuIcons";
 
-interface IState {}
+const drawerWidth = 240;
 
-class Header extends React.Component<IProps, IState> {
-  constructor(props: Readonly<IProps>) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <AppBar position="static">
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex"
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(["width", "margin"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      })
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(["width", "margin"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen
+      })
+    },
+    menuButton: {
+      marginRight: 36
+    },
+    hide: {
+      display: "none"
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: "nowrap"
+    },
+    drawerOpen: {
+      width: drawerWidth,
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen
+      })
+    },
+    drawerClose: {
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      }),
+      overflowX: "hidden",
+      width: theme.spacing(7) + 1,
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9) + 1
+      }
+    },
+    toolbar: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-end",
+      padding: theme.spacing(0, 1),
+      ...theme.mixins.toolbar
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3)
+    }
+  })
+);
+
+export default function Header() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [isOpen, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: isOpen
+        })}
+      >
         <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: isOpen
+            })}
+          >
+            <MenuIcon style={{ color: "white" }} />
+          </IconButton>
           <IconButton edge="start" color="inherit" aria-label="menu">
             <HomeIcon style={{ color: "white" }} />
           </IconButton>
-          <Typography variant="h6">News</Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-    );
-  }
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: isOpen,
+          [classes.drawerClose]: !isOpen
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: isOpen,
+            [classes.drawerClose]: !isOpen
+          })
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </div>
+        <SideMenuIcons />
+      </Drawer>
+    </div>
+  );
 }
-
-export default Header;
